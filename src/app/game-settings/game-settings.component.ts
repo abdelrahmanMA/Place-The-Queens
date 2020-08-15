b61e121e07731719e01a276edecaead7311c82ab
abdelrahmanMA <bme.abdelrahmanmuhammed@gmail.com>
2020-08-15 19:46:41 +0200

    add components and routing

---
+++ src/app/game-settings/game-settings.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-game-settings',
+  templateUrl: './game-settings.component.html',
+  styleUrls: ['./game-settings.component.sass']
+})
+export class GameSettingsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
