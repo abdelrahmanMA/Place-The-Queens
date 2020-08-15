a72ae22b001f06887d36ca7b5222a54252db1b66
abdelrahmanMA <bme.abdelrahmanmuhammed@gmail.com>
2020-08-15 22:01:52 +0200

    router configuration

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,12 +1,19 @@
 import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
+import { RouterModule, Routes } from '@angular/router';
+import { AppComponent } from './app.component';
+import { RulesComponent } from './rules/rules.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { GameSettingsComponent } from './game-settings/game-settings.component';
+import { GameComponent } from './game/game.component';
 
-
+const routes: Routes = [
+  { path: 'rules', component: RulesComponent },
+  { path: 'game-settings', component: GameSettingsComponent },
+  { path: 'game', component: GameComponent }
+];
 
 @NgModule({
-  declarations: [],
-  imports: [
-    CommonModule
-  ]
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
 })
 export class AppRoutingModule { }
